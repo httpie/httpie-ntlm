ce5715df1a8b0daef1885a6b35e76759ab1e6af2
Jakub Roztocil <jakub@roztocil.name>
2013-09-23 16:10:15 +0200

    Fixed installation issue.

--- httpie_ntlm.py
+++ httpie_ntlm.py
@@ -3,10 +3,9 @@ NTLM auth plugin for HTTPie.
 
 """
 from httpie.plugins import AuthPlugin
-from requests_ntlm import HttpNtlmAuth
 
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 __author__ = 'Jakub Roztocil'
 __licence__ = 'BSD'
 
@@ -18,4 +17,5 @@ class NTLMAuthPlugin(AuthPlugin):
     description = ''
 
     def get_auth(self, username, password):
+        from requests_ntlm import HttpNtlmAuth
         return HttpNtlmAuth(username, password)
